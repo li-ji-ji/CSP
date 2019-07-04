689058c03a2731b8f93ec0c9c752903a54786985
NULL <NULL@DESKTOP-8KNB3JC>
2019-07-04 11:26:35 +0800

    csp项目初始化
    CSP项目初始化-lhj

---
+++ src/test/java/com/csp/lhj/CspApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csp.lhj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
